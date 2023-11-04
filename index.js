f0e3b9dccbfd036d3970feb6c6c332aeef81a5e2
abubakr <138801760+abubakr0821@users.noreply.github.com>
2023-11-04 16:07:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//1
+document.querySelector('#demo').innerHTML = 'hello dom'
+
+//2
+document.querySelector('#box').style.color = 'red'
+
+//3
+document.getElementById("box").style.color = "blue"
+document.getElementById("box").style.fontSize = "xx-large"
